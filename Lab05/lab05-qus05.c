c5d2b12664f056dc6c89aefc4486a41b7f9fc10c
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-06-24 01:48:45 +0600

    Add files via upload

--- Lab05/lab05-qus05.c
+++ Lab05/lab05-qus05.c
@@ -1,58 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX 50>
+#define MAX 50
+char q[MAX],f=-1,r=-1;
 
-
- char q[MAX], f=-1, r=-1;
-
-void eng(char value){
-    if(r==MAX-1){
-        printf("Queue is full and hence cannot insert");
-    }
-    else if(f == -1 && r == -1){
-        f = r = 0;
-    }
-    else{
-        r = r + 1;
-        q[r] = value;
-    }
-
+void enq(char val){
+    if(r == MAX-1)
+        printf("queue is full and hence cannot insert");
+        else if(f == -1 && r == -1)
+            f=r=0;
+        else
+            r=r+1;
+            q[r]=val;
 }
-
 char deq(){
-    char value;
-    if(f == -1){
-        printf("Queue is empty, and hence cannot delete");
-    }
-    else{
-        value = q[f];
-        if(f == r){
-            f = r = -1;
-        }
+    char val;
+        if(f == -1)
+            printf("queue is empty and hence cannot delete");
         else{
-            f = f+1;
-        }
-    }
-    return value;
-
+            val = q[f];
+            if(f == r)
+                f=r=-1;
+            else
+                f=f+1;
+                }
+        return val;
 }
-
-
-
-int main()
-{
+int main(){
     int i;
     char s[MAX];
     gets(s);
-    for(i = 0 ; s[i] != '\0' ; i++){
-        if(s[i]!= ''){
-            eng(s[i]);
-        }
+    for(i=0;s[i]!='\0';i++){
+        if(s[i] != ' ')
+            enq(s[i]);
     }
-
-    for(i = f ; i <= r ; i++){
-        s[i] = deq();
-        s[i] = '\0';
+    for(i= f;i<=r;i++)
+        s[i]=deq();
+        s[i]='\0';
         puts(s);
-    }
+
 }
